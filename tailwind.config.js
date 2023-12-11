c62c7bac3940a1e9dcc651fb50ce8b5f30d6bf41
franco giovannangelo <fcgiova@gmail.com>
2023-12-11 12:17:16 -0300

    update of Portfolio project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,13 @@
-require("dotenv").config();
-const enablePurge = process.env.ENABLE_PURGE || false;
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: [],
   purge: [],
   purge: {
-    enabled: enablePurge,
-    content: ["./src/**/*.html", "./src/**/*.scss"],
+    enabled: process.env.ENABLE_PURGE || false,
+    content: ['./src/**/*.html', './src/**/*.scss'],
   },
   theme: {
     extend: {},
   },
-  variants: {},
   plugins: [],
 };

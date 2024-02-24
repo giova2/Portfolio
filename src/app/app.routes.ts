8f982519842e5a456053f7327ea18634512519e7
franco giovannangelo <fcgiova@gmail.com>
2024-02-24 16:26:02 -0300

    netlify build

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { ProjectsComponent } from './projects/projects.component';
-
-export const routes: Routes = [
-  { path: '', component: ProjectsComponent },
-  { path: 'projects/:id', component: ProjectsComponent },
-];
